583ea82a89f952728b27c02134d8eb4482c5034d
hckcrow <65012369+hckcrow@users.noreply.github.com>
2021-03-12 21:45:48 +0700

    upd 12march 21.00 : 1/2 jadi

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function navbar() {
      submenu.classList.toggle('cancel'); 
   });
 }
+
 function form() { 
   window.addEventListener("DOMContentLoaded", function () {
     // get the form elements defined in your form HTML above
@@ -53,5 +54,35 @@ function form() {
       }
     };
     xhr.send(data);
+  }
+}
+
+function searchIt() {
+  const searchBox = document.querySelector(".search-box");
+  const searchBtn = document.querySelector(".search-icon");
+  const cancelBtn = document.querySelector(".cancel-icon");
+  const searchInput = document.querySelector("input");
+  const searchData = document.querySelector(".search-data");
+  searchBtn.onclick =()=>{
+    searchBox.classList.add("active");
+    searchBtn.classList.add("active");
+    searchInput.classList.add("active");
+    cancelBtn.classList.add("active");
+    searchInput.focus();
+    if(searchInput.value != ""){
+      var values = searchInput.value;
+      searchData.classList.remove("active");
+      searchData.innerHTML = "You just typed " + "<span style='font-weight: 500;'>" + values + "</span>";
+    }else{
+      searchData.textContent = "";
+    }
+  }
+  cancelBtn.onclick =()=>{
+    searchBox.classList.remove("active");
+    searchBtn.classList.remove("active");
+    searchInput.classList.remove("active");
+    cancelBtn.classList.remove("active");
+    searchData.classList.toggle("active");
+    searchInput.value = "";
   }
 }
